320eb68e5f8a95ca8d52373fa1497f5668774c81
awotism <callmeoniichandesu@gmail.com>
2020-10-22 13:08:34 +0700

    fix public

--- middlewares/lang.js
+++ middlewares/lang.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const endpoint = 'http://ip-api.com/json/';
 module.exports = async (req, res, next) => {
   let ip = req.socket.remoteAddress;
+  console.log(ip)
   try {
     if (ip == '::ffff:127.0.0.1') {
       req.language = {
