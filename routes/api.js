590fac611a29ac7abdd204a017b51886b34c8cb8
0w <callmeoniichandesu@gmail.com>
2020-10-23 08:23:11 +0700

    Handling API error

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const { updateMain, getArticle } = require('../lib');
+const { Submission } = require('../models');
 router.get('/', (req, res) => {
   res.json({ ok: true });
 });
 
 router.get('/all', (req, res) => {
-  getArticle.all().then(data => res.json(data));
+  getArticle.all()
+  .then(data => res.json(data))
+  .catch(e => {
+    console.log(e);
+    res.status(500).json({ok: false, message: e.message})
+  });
 });
 
 router.get('/update', (req, res) => {
-  updateMain().then((data) => res.status(data.status).json(data));
+  updateMain()
+  .then((data) => res.status(data.status).json(data))
+  .catch(e => {
+    console.log(e);
+    res.status(500).json({ok: false, message: e.message})
+  });
 });
 
+router.get('/submission', (req, res) => {
+  Submission.find({})
+  .then(data => res.json(data))
+  .catch(e => {
+    console.log(e);
+    res.status(500).json({ok: false, message: e.message})
+  });
+});
+
+router.get('/submission/:id', (req, res) => {
+  Submission.findById(req.params.id)
+  .then(data => res.json(data))
+  .catch(e => {
+    console.log(e);
+    res.status(500).json({ok: false, message: e.message})
+  });
+});
+
+router.get('/user/:id', (req, res) => {
+  Submission.find({id: req.params.id})
+  .then(data => res.json(data))
+  .catch(e => {
+    console.log(e);
+    res.status(500).json({ok: false, message: e.message})
+  });
+});
 module.exports = router;
